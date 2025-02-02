a4294d35cbdc7b959d06f98f96121e4a9da682b4
Alexander Rusak <a.rusak@sweedpos.com>
2025-02-02 21:59:24 +0100

    Initial Commit

---
+++ GlucoseMocker/GlucoseMockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlucoseMockerApp.swift
+//  GlucoseMocker
+//
+//  Created by Александр Русак on 02/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct GlucoseMockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GlucoseMockerTests/GlucoseMockerTests.swift
@@ -0,0 +1,17 @@
+//
+//  GlucoseMockerTests.swift
+//  GlucoseMockerTests
+//
+//  Created by Александр Русак on 02/02/2025.
+//
+
+import Testing
+@testable import GlucoseMocker
+
+struct GlucoseMockerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
